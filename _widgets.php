238c441ca73fe94f32a802b36b089471a3ccacc0
franck <carnet.franck.paul@gmail.com>
2012-11-13 15:33:28 +0100

    Add widgets description

--- _widgets.php
+++ _widgets.php
@@ -19,14 +19,14 @@ class seriesWidgets
 	public static function initWidgets($w)
 	{
 		// Widget for all series
-		$w->create('series',__('Series'),array('tplSeries','seriesWidget'));
+		$w->create('series',__('Series'),array('tplSeries','seriesWidget'),null,__('List of series'));
 		$w->series->setting('title',__('Title:'),__('Series'));
 		$w->series->setting('limit',__('Limit (empty means no limit):'),'20');
 		$w->series->setting('sortby',__('Order by:'),'meta_id_lower','combo',
-			array(__('Serie name') => 'meta_id_lower', __('Entries count') => 'count')
+			array(__('Serie name') => 'meta_id_lower',__('Entries count') => 'count')
 			);
 		$w->series->setting('orderby',__('Sort:'),'asc','combo',
-			array(__('Ascending') => 'asc', __('Descending') => 'desc')
+			array(__('Ascending') => 'asc',__('Descending') => 'desc')
 			);
 		$w->series->setting('allserieslinktitle',__('Link to all series:'),__('All series'));
 		$w->series->setting('homeonly',__('Display on:'),0,'combo',
@@ -39,20 +39,20 @@ class seriesWidgets
 		$w->series->setting('class',__('CSS class:'),'');
 
 		// Widget for currently displayed post
-		$w->create( 'seriesPosts', __( 'Siblings' ), array( 'tplSeries', 'seriePostsWidget' ) );
-		$w->seriesPosts->setting( 'title', __( 'Title:' ),__( 'Siblings' ) );
+		$w->create('seriesPosts',__('Siblings'),array('tplSeries','seriePostsWidget'),null,__('Other posts of the same serie(s)'));
+		$w->seriesPosts->setting('title',__( 'Title:' ),__( 'Siblings' ));
 		$w->seriesPosts->setting('serietitle',__('Show titles of series'),1,'check');
 		$w->seriesPosts->setting('orderseriesby',__('Order series by:'),'asc','combo',
-			array(__('Ascending') => 'asc', __('Descending') => 'desc')
+			array(__('Ascending') => 'asc',__('Descending') => 'desc')
 			);
 		$w->seriesPosts->setting('current',__('Include current entry:'),'std','combo',
-			array(__('Standard') => 'std', __('With link') => 'link', __('None') => 'none')
+			array(__('Standard') => 'std',__('With link') => 'link',__('None') => 'none')
 			);
 		$w->seriesPosts->setting('sortentriesby',__('Order entries by:'),'date','combo',
-			array(__('Date') => 'date', __('Entry title') => 'title')
+			array(__('Date') => 'date',__('Entry title') => 'title')
 			);
 		$w->seriesPosts->setting('orderentriesby',__('Sort:'),'asc','combo',
-			array(__('Ascending') => 'asc', __('Descending') => 'desc')
+			array(__('Ascending') => 'asc',__('Descending') => 'desc')
 			);
 		$w->seriesPosts->setting('homeonly',__('Display on:'),0,'combo',
 			array(
