0c07a8d5720bfeb3493aecf0be27ae07303beb98
franck <carnet.franck.paul@gmail.com>
2013-09-24 22:40:04 +0200

    serie_posts.php now works with new actions.

--- serie_posts.php
+++ serie_posts.php
@@ -61,34 +61,11 @@ try {
 	$core->error->add($e->getMessage());
 }
 
-# Actions combo box
-$combo_action = array();
-if ($core->auth->check('publish,contentadmin',$core->blog->id))
-{
-	$combo_action[__('Status')] = array(
-		__('Publish') => 'publish',
-		__('Unpublish') => 'unpublish',
-		__('Schedule') => 'schedule',
-		__('Mark as pending') => 'pending'
-	);
-}
-$combo_action[__('Mark')] = array(
-	__('Mark as selected') => 'selected',
-	__('Mark as unselected') => 'unselected'
-);
-$combo_action[__('Change')] = array(__('Change category') => 'category');
-if ($core->auth->check('admin',$core->blog->id))
-{
-	$combo_action[__('Change')] = array_merge($combo_action[__('Change')],
-		array(__('Change author') => 'author'));
-}
-if ($core->auth->check('delete,contentadmin',$core->blog->id))
-{
-	$combo_action[__('Delete')] = array(__('Delete') => 'delete');
-}
+$posts_actions_page = new dcPostsActionsPage($core,'plugin.php',array('p'=>'series', 'm'=>'serie_posts', 'serie'=> $serie));
 
-# --BEHAVIOR-- adminPostsActionsCombo
-$core->callBehavior('adminPostsActionsCombo',array(&$combo_action));
+if ($posts_actions_page->process()) {
+	return;
+}
 
 ?>
 <html>
@@ -153,7 +130,7 @@ if (!$core->error->flag())
 	# Show posts
 	echo '<h3>'.__('List of entries in this serie').'</h3>';
 	$post_list->display($page,$nb_per_page,
-	'<form action="posts_actions.php" method="post" id="form-entries">'.
+	'<form action="plugin.php" method="post" id="form-entries">'.
 
 	'%s'.
 
@@ -161,11 +138,12 @@ if (!$core->error->flag())
 	'<p class="col checkboxes-helpers"></p>'.
 
 	'<p class="col right"><label for="action" class="classic">'.__('Selected entries action:').'</label> '.
-	form::combo('action',$combo_action).
+	form::combo('action',$posts_actions_page->getCombo()).
 	'<input type="submit" value="'.__('ok').'" /></p>'.
 	form::hidden('post_type','').
-	form::hidden('redir',$p_url.'&amp;m=serie_posts&amp;serie='.
-		str_replace('%','%%',rawurlencode($serie)).'&amp;page='.$page).
+	form::hidden('p','series').
+	form::hidden('m','serie_posts').
+	form::hidden('serie',$serie).
 	$core->formNonce().
 	'</div>'.
 	'</form>');
