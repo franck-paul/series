c9274423475908f1293968d113f02d0aa839f2db
franck <carnet.franck.paul@gmail.com>
2021-10-23 12:44:43 +0200

    Add alternative repository

--- _define.php
+++ _define.php
@@ -20,14 +20,16 @@ $this->registerModule(
     'Franck Paul',     // Author
     '1.1',             // Version
     [
-        'requires'    => [['core', '2.19']],                          // Dependencies
-        'permissions' => 'usage,contentadmin',                        // Permissions
-        'priority'    => 1001,                                        // Must be higher than dcLegacyEditor/dcCKEditor priority (ie 1000)
-        'type'        => 'plugin',                                    // Type
-        'details'     => 'https://open-time.net/docs/plugins/series', // Details URL
-        'support'     => 'https://github.com/franck-paul/series',     // Support URL
+        'requires'    => [['core', '2.19']],        // Dependencies
+        'permissions' => 'usage,contentadmin',      // Permissions
+        'priority'    => 1001,                      // Must be higher than dcLegacyEditor/dcCKEditor priority (ie 1000)
+        'type'        => 'plugin',                  // Type
         'settings'    => [
             'pref' => '#user-options.series_prefs'
-        ]
+        ],
+
+        'details'    => 'https://open-time.net/docs/plugins/series',       // Details URL
+        'support'    => 'https://github.com/franck-paul/series',            // Support URL
+        'repository' => 'https://raw.githubusercontent.com/franck-paul/series/main/dcstore.xml'
     ]
 );
