a2e3689458558b34a1accf314cec9ead2fe521ee
franck <carnet.franck.paul@gmail.com>
2023-10-25 09:48:35 +0200

    New release 5.2

--- _define.php
+++ _define.php
@@ -14,7 +14,7 @@ $this->registerModule(
     'Series',
     'Series of posts',
     'Franck Paul',
-    '5.1',
+    '5.2',
     [
         'requires'    => [['core', '2.28']],
         'permissions' => 'My',
