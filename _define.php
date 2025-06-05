5aee087f4cbfd8853a241cf82d5e48496a2583f9
franck <carnet.franck.paul@gmail.com>
2025-06-05 14:36:09 +0200

    New release 7.5

--- _define.php
+++ _define.php
@@ -15,9 +15,9 @@ $this->registerModule(
     'Series',
     'Series of posts',
     'Franck Paul',
-    '7.4',
+    '7.5',
     [
-        'date'     => '2025-06-01T08:09:41+0200',
+        'date'     => '2025-06-05T14:36:09+0200',
         'requires' => [
             ['core', '2.34'],
             ['TemplateHelper'],
