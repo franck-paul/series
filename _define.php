dae68287acac9944447709aecb99e91a20e3d94c
franck <contact@open-time.net>
2026-04-06 15:17:16 +0200

    New release 9.4

--- _define.php
+++ _define.php
@@ -15,9 +15,9 @@ $this->registerModule(
     'Series',
     'Series of posts',
     'Franck Paul',
-    '9.3',
+    '9.4',
     [
-        'date'     => '2026-04-01T13:54:54+0200',
+        'date'     => '2026-04-06T15:17:16+0200',
         'requires' => [
             ['core', '2.37'],
             ['TemplateHelper'],
